e279936455ef2e932666e082aebb5e07c629354d
Adam Krizar <48439304+ApocalypseArisen@users.noreply.github.com>
2022-05-17 18:37:32 +0200

    clean up + display data color coded

--- frontend.py
+++ frontend.py
@@ -3,7 +3,9 @@ import pandas as pd
 import sys
 
 df = pd.read_csv(sys.argv[1], sep="\t")
-df.columns = ['x', 'y', 'data']
+df.columns = ['x', 'y', 'data', 'source']
 
-fig = px.scatter(df, x="x", y="y", hover_data=['data'])
+# print(str(df))
+
+fig = px.scatter(df, x="x", y="y", hover_data=['data'], color="source")
 fig.show()
