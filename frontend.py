57295d769046b5309ccb85dc9e07ba2764b3e515
Adam Krizar <48439304+ApocalypseArisen@users.noreply.github.com>
2022-05-22 18:45:00 +0200

    add second plot for additional tags

--- frontend.py
+++ frontend.py
@@ -1,11 +1,10 @@
-import plotly.graph_objects as go
-import plotly.express.colors as colors
-
 import csv
 import sys
 
+import plotly.graph_objects as go
 
-class TracedSource:
+
+class TracedObject:
     def __init__(self, name, x=[], y=[], title=[]):
         self.name = name
         self.x = x
@@ -14,49 +13,46 @@ class TracedSource:
 
 
 traced_sources = {}
+traced_tags = {}
 
 with open(sys.argv[1], newline='') as umap_vectors:
     csv_reader = csv.reader(umap_vectors, delimiter='\t')
     for row in csv_reader:
+        tags = row[4].replace('[', '').replace(']', '').replace('\'', '').lower()
+        tag_list = list(tags.split(', '))
+
+        for tag in tag_list:
+            if tag in traced_tags:
+                traced_tags[tag].x.append(float(row[0]))
+                traced_tags[tag].y.append(float(row[1]))
+                traced_tags[tag].title.append(row[2])
+            else:
+                traced_tags[tag] = TracedObject(tag, [float(row[0])], [float(row[1])], [row[2]])
+
         if row[3] in traced_sources:
             traced_sources[row[3]].x.append(float(row[0]))
             traced_sources[row[3]].y.append(float(row[1]))
             traced_sources[row[3]].title.append(row[2])
         else:
-            traced_sources[row[3]] = TracedSource(row[3], [float(row[0])], [float(row[1])], [row[2]])
+            traced_sources[row[3]] = TracedObject(row[3], [float(row[0])], [float(row[1])], [row[2]])
 
 fig = go.Figure()
-buttons = []
-button = dict(label = 'show all',
-              method = 'update',
-              args = [{
-                  'visible': True * len(traced_sources),
-                  'title': 'all'
-              }])
-buttons.append(button)
+fig2 = go.Figure()
 
-i = 0
 for source in traced_sources:
     fig.add_trace(go.Scatter(
         x=traced_sources[source].x, y=traced_sources[source].y,
         hovertext=traced_sources[source].title,
         mode='markers',
-        name=traced_sources[source].name,
-        marker=dict(color=colors.qualitative.Alphabet[i])
-    ))
+        name=traced_sources[source].name))
 
-    switches = [False] * len(traced_sources)
-    switches[i] = True
+for tag in traced_tags:
+    fig2.add_trace(go.Scatter(
+        x=traced_tags[tag].x, y=traced_tags[tag].y,
+        hovertext=traced_tags[tag].title,
+        mode='markers',
+        name=traced_tags[tag].name))
 
-    button = dict(label = 'show ' + traced_sources[source].name,
-                  method = 'update',
-                  args = [{
-                      'visible': switches,
-                      'title': traced_sources[source].name
-                  }])
 
-    buttons.append(button)
-    i = i + 1
-
-fig.update_layout(updatemenus=[dict(buttons=buttons)])
 fig.show()
+fig2.show()
